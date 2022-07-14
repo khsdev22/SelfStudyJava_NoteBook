f863aac4c3a31238fb18bba408447ac907b124bd
khsdev22 <khsdev22@gmail.com>
2022-07-14 16:33:55 +0900

    인터페이스

---
+++ SelfStudyJava/src/sec01/exam04/RemoteControlExample.java
@@ -0,0 +1,13 @@
+//package sec01.exam04;
+//
+//import sec01.exam01.RemoteControl;
+//
+//public class RemoteControlExample {
+//	
+//	public static void main(String[] args) {
+//		RemoteControl rc;
+//		rc = new Television();
+//		rc = new Audio();
+//	}
+//
+//}
